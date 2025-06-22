2ed7453b6d171a0a967dbbbd89cc7b9edd98a4ca
salmon <elis.staaf@proton.me>
2025-06-22 19:38:44 +0200

    fix: Change name of upstream repo extensions

--- distrohop/scripts/distrohop-install-editor.sh
+++ distrohop/scripts/distrohop-install-editor.sh
@@ -2,12 +2,12 @@
 
 # Use snaptoken's repo, as it's basically Antirez's repo but with some
 # improvements. Thanks to Antirez, anyways, since he made original kilo.
-git clone https://github.com/snaptoken/kilo-src kilo.distrohop
+git clone https://github.com/snaptoken/kilo-src kilo.upstream_repo.distrohop
 
 # Build the project to ``distrohop-kilo``
-cd kilo.distrohop
+cd kilo.upstream_repo.distrohop
 gcc kilo.c -o /usr/local/bin/distrohop-kilo
 
 # Remove any traces of the upstream repo.
 cd ..
-rm -rf kilo.distrohop
+rm -rf kilo.upstream_repo.distrohop
